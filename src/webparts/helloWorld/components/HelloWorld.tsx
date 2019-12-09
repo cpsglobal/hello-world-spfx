68ae07e70698bf2fef12d22bf198541d8956d05c
Paul Perry <Paul.Perry@CPS.co.uk>
2019-12-09 17:37:37 +0000

    Add React Hooks example

--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styles from './HelloWorld.module.scss';
-
+import { PrimaryButton } from 'office-ui-fabric-react';
 export interface IHelloWorldProps {
   title: string;
   subTitle: string;
@@ -9,8 +9,10 @@ export interface IHelloWorldProps {
   buttonTarget: string;
 }
 
-export const HelloWorld: React.FunctionComponent<IHelloWorldProps> = (props) =>
-  <>
+export const HelloWorld: React.FunctionComponent<IHelloWorldProps> = (props) => {
+  const [count, setCount] = React.useState(0);
+
+  return (
     <div className={styles.helloWorld}>
       <div className={styles.container}>
         <div className={styles.row}>
@@ -23,6 +25,13 @@ export const HelloWorld: React.FunctionComponent<IHelloWorldProps> = (props) =>
             </a>
           </div>
         </div>
+        <div className={styles.row}>
+          <div className={styles.column}>
+            <PrimaryButton text="Click Me" onClick={() => setCount(count + 1)} />
+            <div id="timesClicked">You clicked {count} times.</div>
+          </div>
+        </div>
       </div>
     </div>
-  </>;
+  );
+};
